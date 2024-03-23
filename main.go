6315062a4ce28cb62b7ac40155fc372c14f2a7a7
Sarun M <sarun.maksuanpan@krungsrinimble.com>
2024-03-23 22:38:11 +0700

    refactor

--- main.go
+++ main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 type Users struct {
-    ID   int    `json:"id"`
-    Name string `json:"name"`
-    Age  int    `json:"age"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 var User = []Users{
-    {ID: 1, Name: "John Doe", Age: 25},
+	{ID: 1, Name: "John Doe", Age: 25},
 }
 
 func main() {
-    http.HandleFunc("/", handle)
+	http.HandleFunc("/", handle)
 
-    log.Println("server started at :8080")
-    log.Fatal(http.ListenAndServe("localhost:8080", nil))
-    log.Println("server stopped")
+	log.Println("server started at :8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	log.Println("server stopped")
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodPost {
-        w.WriteHeader(http.StatusCreated)
-        w.Write([]byte(`"test POST"`))
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusCreated)
 
-        b, err := json.Marshal(User)
-        if err != nil {
-            log.Println(err)
-            return
-        }
-        return
-    }
+		b, err := json.Marshal(User)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-    w.Write([]byte(`{"word":"Hello, World!!!"}`))
+		w.Write(b)
+		return
+	}
+
+	w.Write([]byte(`{"word":"Hello, World!!!"}`))
 }
