42b3b23d3ad1608f2919991d4fdea105a051ac4b
Sarun M <sarun.maksuanpan@krungsrinimble.com>
2024-03-27 22:59:53 +0700

    update api to select/insert data in db

--- echoapi/go.mod
+++ echoapi/go.mod
@@ -2,7 +2,10 @@ module github.com/sarunm/followUp
 
 go 1.22.1
 
-require github.com/labstack/echo/v4 v4.11.4
+require (
+	github.com/labstack/echo/v4 v4.11.4
+	github.com/lib/pq v1.10.9
+)
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
--- echoapi/server.go
+++ echoapi/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"time"
@@ -49,8 +52,8 @@ func main() {
 		return false, nil
 	}))
 
-	g.GET("/users", getUserHandler)
-	g.POST("/users", createUserHandler)
+	g.GET("/users", getUsersFromDB)
+	g.POST("/users", createUserToDbHandler)
 	g.GET("/health", healthHandler)
 	e.Start(":8080")
 
@@ -70,6 +73,75 @@ func createUserHandler(c echo.Context) error {
 	}
 	User = append(User, u)
 	return c.JSON(http.StatusCreated, u)
+}
+
+func createUserToDbHandler(c echo.Context) error {
+
+	u := Users{}
+	if err := c.Bind(&u); err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+	}
+
+	url := "postgres://dxrrvvah:2RyYR4ZuPvwebT8E8q6gS0qMZkJc0PKT@john.db.elephantsql.com/dxrrvvah"
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		fmt.Println("connect to database error", err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO users (name, age) VALUES ($1, $2) RETURNING id")
+	if err != nil {
+		fmt.Println("can't prepare query", err)
+	}
+
+	row := stmt.QueryRow(u.Name, u.Age)
+
+	err = row.Scan(&u.ID)
+	if err != nil {
+		fmt.Println("can't scan id", err)
+
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
+func getUsersFromDB(c echo.Context) error {
+	url := "postgres://dxrrvvah:2RyYR4ZuPvwebT8E8q6gS0qMZkJc0PKT@john.db.elephantsql.com/dxrrvvah"
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		fmt.Println("connect to database error", err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT * FROM users")
+	if err != nil {
+		fmt.Println("can't prepare query", err)
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		fmt.Println("can't query all rows ", err)
+	}
+
+	var users = []Users{}
+
+	if err != nil {
+		fmt.Println("can't scan row into variables", err)
+	}
+	for rows.Next() {
+
+		var u Users
+		err = rows.Scan(&u.ID, &u.Name, &u.Age)
+
+		//err = rows.Scan(&user)
+		if err != nil {
+			fmt.Println("can't scan row into variables", err)
+		}
+		fmt.Println("id", u.ID, "name", u.Name, "age", u.Age)
+
+		users = append(users, u)
+	}
+
+	return c.JSON(http.StatusOK, users)
 }
 
 func healthHandler(c echo.Context) error {
