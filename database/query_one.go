4b57711163c50bc82c3ad69478aad0120916c42a
Sarun M <sarun.maksuanpan@krungsrinimble.com>
2024-03-27 22:27:40 +0700

    add update db function

--- database/query_one.go
+++ database/query_one.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
 
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-    url := "postgres://dxrrvvah:2RyYR4ZuPvwebT8E8q6gS0qMZkJc0PKT@john.db.elephantsql.com/dxrrvvah"
+	url := "postgres://dxrrvvah:2RyYR4ZuPvwebT8E8q6gS0qMZkJc0PKT@john.db.elephantsql.com/dxrrvvah"
 
-    db, err := sql.Open("postgres", url)
-    if err != nil {
-        fmt.Println("connect to database error", err)
-    }
-    defer db.Close()
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		fmt.Println("connect to database error", err)
+	}
+	defer db.Close()
 
-    stmt, err := db.Prepare("SELECT * FROM users where id=$1")
-    if err != nil {
-        fmt.Println("can't prepare query ", err)
-    }
+	stmt, err := db.Prepare("SELECT * FROM users where id=$1")
+	if err != nil {
+		fmt.Println("can't prepare query ", err)
+	}
 
-    rowId := 1
-    row := stmt.QueryRow(rowId)
-    var id int
-    var name string
-    var age int
-    err = row.Scan(&id, &name, &age)
-    if err != nil {
-        fmt.Println("can't scan row into variables", err)
-    }
-    fmt.Println("id", id, "name", name, "age", age)
+	rowId := 1
+	row := stmt.QueryRow(rowId)
+	var id int
+	var name string
+	var age int
+	err = row.Scan(&id, &name, &age)
+	if err != nil {
+		fmt.Println("can't scan row into variables", err)
+	}
+	fmt.Println("id", id, "name", name, "age", age)
 }
---
+++ database/update.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/lib/pq"
+)
+
+func main() {
+    url := "postgres://dxrrvvah:2RyYR4ZuPvwebT8E8q6gS0qMZkJc0PKT@john.db.elephantsql.com/dxrrvvah"
+
+    db, err := sql.Open("postgres", url)
+    if err != nil {
+        fmt.Println("connect to database error", err)
+    }
+    defer db.Close()
+
+    stmt, err := db.Prepare("UPDATE users SET name=$2, age=$3 WHERE id=$1")
+    if err != nil {
+        fmt.Println("can't prepare query", err)
+    }
+
+    if _, err = stmt.Exec(1, "nick", 12); err != nil {
+        fmt.Println("can't update row", err)
+    }
+    fmt.Println("update row success")
+
+}
